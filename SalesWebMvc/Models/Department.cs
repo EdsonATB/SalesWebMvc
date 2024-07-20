9873d341751411cf140caab697446e8c8739f285
Edson Augusto <edsonaugusto2006@gmail.com>
2024-07-20 18:46:39 -0300

    Other entities and second migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace SalesWebMvc.Models
 {
@@ -9,8 +10,26 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
+        public Department()
+        {
+        }
 
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
 
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(Seller => Seller.TotalSales(initial, final));
+        }
     }
 }
