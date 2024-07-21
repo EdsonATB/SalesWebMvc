0afacfeea6a37a6d87b611a0203075c703df330c
Edson Augusto <edsonaugusto2006@gmail.com>
2024-07-21 19:01:39 -0300

    SellerFormViewModel and Department select component

---
+++ SalesWebMvc/Models/ViewModels/SellerFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+
+namespace SalesWebMvc.Models.ViewModels
+{
+    public class SellerFormViewModel
+    {
+        public Seller Seller { get; set; }
+        public ICollection<Department> Departments { get; set; }
+
+
+    }
+}
